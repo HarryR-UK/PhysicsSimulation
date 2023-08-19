1fa1961d8d0763b44bb1732c8fd0c73f448bbabe
WindowBender <justcallitaday@outlook.com>
2023-08-19 16:33:26 +0100

    colors

--- build/compile.sh
+++ build/compile.sh
@@ -1,3 +1,3 @@
-g++ -std=c++20 ../src/*.cpp -o Physics -I/opt/homebrew/Cellar/sfml/3.6.0/include -L/opt/homebrew/Cellar/sfml/2.6.0/lib -lsfml-window -lsfml-graphics -lsfml-network -lsfml-system -lsfml-audio
+g++ -std=c++20 ../src/*.cpp -o ../bin/Physics -I/opt/homebrew/Cellar/sfml/3.6.0/include -L/opt/homebrew/Cellar/sfml/2.6.0/lib -lsfml-window -lsfml-graphics -lsfml-network -lsfml-system -lsfml-audio
 
-./Physics
+../bin/Physics
